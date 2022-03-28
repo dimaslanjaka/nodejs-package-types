8d887817c57926173070e8574238437aa91040b3
dimaslanjaka <dimaslanjaka@gmail.com>
2022-03-28 22:04:43 +0700

    fix

--- index.d.ts
+++ index.d.ts
@@ -12,30 +12,36 @@
 
 import stream = require('stream');
 
+interface DynamicObject {
+  [keys: string]: any;
+  path?: any;
+  base?: any;
+}
+
 declare function through2(transform?: through2.TransformFunction, flush?: through2.FlushCallback): stream.Transform;
 declare function through2(opts?: stream.DuplexOptions, transform?: through2.TransformFunction, flush?: through2.FlushCallback): stream.Transform;
 
 declare namespace through2 {
-    interface Through2Constructor extends stream.Transform {
-        new (opts?: stream.DuplexOptions): stream.Transform;
-        (opts?: stream.DuplexOptions): stream.Transform;
-    }
+  interface Through2Constructor extends stream.Transform {
+    new(opts?: stream.DuplexOptions): stream.Transform;
+    (opts?: stream.DuplexOptions): stream.Transform;
+  }
 
-    type TransformCallback = (err?: any, data?: any) => void;
-    type TransformFunction = (this: stream.Transform, chunk: any, enc: BufferEncoding, callback: TransformCallback) => void;
-    type FlushCallback = (this: stream.Transform, flushCallback: () => void) => void;
+  type TransformCallback = (err?: any, data?: any) => void;
+  type TransformFunction = (this: stream.Transform, chunk: DynamicObject, enc: BufferEncoding, callback: TransformCallback) => void;
+  type FlushCallback = (this: stream.Transform, flushCallback: () => void) => void;
 
-    /**
-     * Convenvience method for creating object streams
-     */
-    function obj(transform?: TransformFunction, flush?: FlushCallback): stream.Transform;
+  /**
+   * Convenvience method for creating object streams
+   */
+  function obj(transform?: TransformFunction, flush?: FlushCallback): stream.Transform;
 
-    /**
-     * Creates a constructor for a custom Transform. This is useful when you
-     * want to use the same transform logic in multiple instances.
-     */
-    function ctor(transform?: TransformFunction, flush?: FlushCallback): Through2Constructor;
-    function ctor(opts?: stream.DuplexOptions, transform?: TransformFunction, flush?: FlushCallback): Through2Constructor;
+  /**
+   * Creates a constructor for a custom Transform. This is useful when you
+   * want to use the same transform logic in multiple instances.
+   */
+  function ctor(transform?: TransformFunction, flush?: FlushCallback): Through2Constructor;
+  function ctor(opts?: stream.DuplexOptions, transform?: TransformFunction, flush?: FlushCallback): Through2Constructor;
 }
 
 export = through2;
