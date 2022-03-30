29151bf41611b8c7088f6b5f806ff3aab5198fa3
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-03-30 18:22:34 +0700

    Update

--- push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-remote_repo=https://github.com/dimaslanjaka/nodejs-package-types.git
-INPUT_BRANCH=through2
-git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags;
-git subtree push -m 'update' --squash --rejoin --prefix through2 origin through2
-git subtree push --prefix=through2 https://github.com/dimaslanjaka/nodejs-package-types.git through2
