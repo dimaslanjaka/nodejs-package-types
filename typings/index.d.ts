f846d523b529a5e6c0a23c75b32f080a27e11f0f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-03 21:16:43 +0700

    +non-typings module

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,7 @@
-declare module 'kill-port' {
-  export default function killPort(port: number, type: 'tcp' | 'udp'): Promise<any>;
-}
+import "./kill-port";
+import "./markdown-it-abbr";
+import "./markdown-it-footnote";
+import "./markdown-it-image-figures";
+import "./markdown-it-mark";
+import "./markdown-it-sub";
+import "./markdown-it-sup";
