097913d73ad8dc1cbb57235876bafc316aea3c19
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-10 08:13:39 +0700

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -35,7 +35,8 @@ const getCache = () => require('./node_modules/.cache/npm-install.json');
  * data['key']='value';
  * saveCache(data)
  */
-const saveCache = (data) => fs.writeFileSync(cacheJSON, JSON.stringify(data, null, 2));
+const saveCache = (data) =>
+  fs.writeFileSync(cacheJSON, JSON.stringify(data, null, 2));
 
 (async () => {
   // @todo clear cache local packages
@@ -94,7 +95,9 @@ const saveCache = (data) => fs.writeFileSync(cacheJSON, JSON.stringify(data, nul
     const exists = toUpdate.map(
       (pkgname) =>
         fs.existsSync(path.join(__dirname, 'node_modules', pkgname)) &&
-        fs.existsSync(path.join(__dirname, 'node_modules', pkgname, 'package.json'))
+        fs.existsSync(
+          path.join(__dirname, 'node_modules', pkgname, 'package.json')
+        )
     );
     //console.log({ exists });
     return exists.every((exist) => exist === true);
@@ -111,12 +114,22 @@ const saveCache = (data) => fs.writeFileSync(cacheJSON, JSON.stringify(data, nul
             toUpdate.push('--check-cache');
           }
         }
+        // yarn cache clean
+        await summon('yarn', ['cache', 'clean'], {
+          cwd: __dirname,
+          stdio: 'inherit'
+        });
         // yarn upgrade package
         await summon('yarn', ['upgrade'].concat(...toUpdate), {
           cwd: __dirname,
           stdio: 'inherit'
         });
       } else {
+        // npm cache clean package
+        await summon('npm', ['cache', 'clean'].concat(...toUpdate), {
+          cwd: __dirname,
+          stdio: 'inherit'
+        });
         // npm update package
         await summon('npm', ['update'].concat(...toUpdate), {
           cwd: __dirname,
@@ -129,17 +142,28 @@ const saveCache = (data) => fs.writeFileSync(cacheJSON, JSON.stringify(data, nul
 
       const argv = process.argv;
       // node postinstall.js --commit
-      if (fs.existsSync(path.join(__dirname, '.git')) && argv.includes('--commit')) {
+      if (
+        fs.existsSync(path.join(__dirname, '.git')) &&
+        argv.includes('--commit')
+      ) {
         await summon('git', ['add', 'package.json'], { cwd: __dirname });
         await summon('git', ['add', 'package-lock.json'], { cwd: __dirname });
         const status = await summon('git', ['status', '--porcelain'], {
           cwd: __dirname
         });
         console.log({ status });
-        if (status.stdout && (status.stdout.includes('package.json') || status.stdout.includes('package-lock.json'))) {
-          await summon('git', ['commit', '-m', 'Update dependencies\nDate: ' + new Date()], {
-            cwd: __dirname
-          });
+        if (
+          status.stdout &&
+          (status.stdout.includes('package.json') ||
+            status.stdout.includes('package-lock.json'))
+        ) {
+          await summon(
+            'git',
+            ['commit', '-m', 'Update dependencies\nDate: ' + new Date()],
+            {
+              cwd: __dirname
+            }
+          );
         }
       }
     } catch (e) {
@@ -161,7 +185,8 @@ function summon(cmd, args = [], opt = {}) {
   const spawnopt = Object.assign({ cwd: __dirname }, opt || {});
   // *** Return the promise
   return new Promise(function (resolve) {
-    if (typeof cmd !== 'string' || cmd.trim().length === 0) return resolve(new Error('cmd empty'));
+    if (typeof cmd !== 'string' || cmd.trim().length === 0)
+      return resolve(new Error('cmd empty'));
     let stdout = '';
     let stderr = '';
     const child = spawn(cmd, args, spawnopt);
@@ -188,7 +213,8 @@ function summon(cmd, args = [], opt = {}) {
 
     child.on('close', function (code) {
       // Should probably be 'exit', not 'close'
-      if (code !== 0) console.log('[ERROR]', cmd, ...args, 'dies with code', code);
+      if (code !== 0)
+        console.log('[ERROR]', cmd, ...args, 'dies with code', code);
       // *** Process completed
       resolve({ stdout, stderr });
     });
