5b81ea2843adecbf321de30d65cc53d644dc3160
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-13 01:20:32 +0700

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -1,4 +1,19 @@
 const fs = require('fs');
 const path = require('path')
+const axios = require('axios')
 
-const packer = path.join(process.cwd(), 'packer.js')
+const packer = path.join(process.cwd(), 'packer.js');
+
+if (!fs.existsSync(packer)) {
+  download('https://raw.githubusercontent.com/dimaslanjaka/nodejs-package-types/main/packer.js', packer);
+}
+
+
+function download(url, output) {
+  axios.get(url, {responseType: 'blob'}).then(response => {
+    fs.writeFile(output, response.data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+  });
+}
