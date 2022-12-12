81d8d6cbe1b5049e8b2fe0586f8ca1d72184a872
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-13 01:43:18 +0700

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -1,25 +1,41 @@
-const fs = require('fs');
-const path = require('path')
-const axios = require('axios')
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
 
-function setup(options = { force: false, includes: ['packer', 'postinstall', 'preinstall', '.github/workflows/build-release.yml'] }) {
+function setup(
+  options = {
+    force: false,
+    includes: [
+      "packer",
+      "postinstall",
+      "preinstall",
+      ".github/workflows/build-release.yml",
+    ],
+  }
+) {
   const { includes } = options;
-  includes.forEach(downloadPath => {
-    const dPath = !downloadPath.endsWith('.js') ? downloadPath + '.js' : downloadPath
+  includes.forEach((downloadPath) => {
+    const dPath = !downloadPath.endsWith(".js")
+      ? downloadPath + ".js"
+      : downloadPath;
     const packer = path.join(process.cwd(), dPath);
     if (!fs.existsSync(packer) || options.force) {
-      download('https://raw.githubusercontent.com/dimaslanjaka/nodejs-package-types/main/'+dPath, path.join(process.cwd(), dPath));
+      download(
+        "https://raw.githubusercontent.com/dimaslanjaka/nodejs-package-types/main/" +
+          dPath,
+        path.join(process.cwd(), dPath)
+      );
     }
-  }); 
+  });
 }
 
 function download(url, output) {
-  axios.get(url, {responseType: 'blob'}).then(response => {
+  axios.get(url, { responseType: "blob" }).then((response) => {
     fs.writeFile(output, response.data, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
+      if (err) throw err;
+      console.log("The file has been saved!");
     });
   });
 }
 
-module.exports = { setup }
+module.exports = { setup };
