3b2ce45f81adb281bacc1372e7134ff5274c6a55
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-29 15:51:21 +0700

    Create typedoc-server.js

---
+++ typedoc-server.js
@@ -0,0 +1,42 @@
+const browserSync = require('browser-sync');
+const { spawn } = require('git-command-helper/dist/spawn');
+const bs = browserSync.create();
+
+/**
+ * Docs serve
+ * required:
+ * * add docs script for `npm run docs`
+ * * npm i -D git-command-helper
+ */
+
+/**
+ * @type {browserSync.MiddlewareHandler | browserSync.PerRouteMiddleware}
+ */
+const buildDocs = async (_req, _res, next) => {
+  await spawn('npm', ['run', 'docs'], { cwd: __dirname, stdio: 'inherit' });
+  next();
+};
+
+// Serve files from 3 directories with serve-static options
+bs.init({
+  cors: true,
+  open: false,
+  port: 4000,
+  serveStatic: ['.', './docs'],
+  serveStaticOptions: {
+    extensions: ['html', 'js', 'css'] // pretty urls
+  },
+  server: {
+    routes: {
+      '/': '/docs',
+      '/docs': '/docs',
+      // common
+      '/node_modules': './node_modules',
+      '/tmp': './tmp',
+      // custom
+      '/js': './tests/js',
+      '/css': './tests/css'
+    },
+    middleware: [{ route: '/docs/safelinkify/demo/', handle: buildDocs }]
+  }
+});
