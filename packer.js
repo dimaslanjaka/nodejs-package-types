20930b1b834662c8d50df58b67d893db7c4e42fe
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-05-12 17:09:34 +0700

    fix: invalid catch return

--- packer.js
+++ packer.js
@@ -265,12 +265,12 @@ async function addReadMe() {
       continue;
     }
     // skip index tarball which ignored by .gitignore
-    const checkIgnore = (
-      await spawnAsync('git', ['status', '--porcelain', '--ignored'], { cwd: __dirname }).catch((e) => {
-        console.log(e);
-        return '';
-      })
-    ).output
+    let checkIgnore = await spawnAsync('git', ['status', '--porcelain', '--ignored'], { cwd: __dirname }).catch((e) => {
+      console.log(e);
+      return { output: '' };
+    });
+
+    checkIgnore = checkIgnore.output
       .split(/\r?\n/)
       .map((str) => str.trim())
       .filter((str) => str.startsWith('!!'))
