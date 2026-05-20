a4097613ad95c3798579d512a5d5efd81f9b6343
dimaslanjaka <dimaslanjaka@gmail.com>
2026-05-20 14:07:55 +0700

    chore: remove hexo,hexo-log,hexo-util

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 /// <reference types="node" />
 /// <reference types="through2" />
 /// <reference path="./typings/index.d.ts" />
-/// <reference path="./hexo/index.d.ts" />
-/// <reference path="./hexo-util/dist/index.d.ts" />
-/// <reference path="./hexo-log/index.d.ts" />
 /// <reference path="./hexo-bunyan/index.d.ts" />
 
 const setup = require('./src/setup');
